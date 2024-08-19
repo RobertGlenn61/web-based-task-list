385ab52ba0638bdac83788c433039da55004f485
RobertGlenn61 <rc_glenn@hotmail.com>
2024-08-19 11:56:51 -0700

    Added new task items

--- main.py
+++ main.py
@@ -9,15 +9,20 @@ def add_task():
     task = gui.session_state['new_task'] + '\n'
     my_tasks.append(task)
     write_todos(my_tasks)
-    gui.session_state['new_task'] = ''
+    del gui.session_state['new_task']
 
 
 gui.title('My Task List')
 gui.subheader("Tasks for ")
 gui.write("regular lines")
 
-for task in my_tasks:
-    streamlit.checkbox(task)
+for index, task in enumerate(my_tasks):
+    checkbox = streamlit.checkbox(task, key=task)
+    if checkbox:
+        my_tasks.pop(index)
+        write_todos(my_tasks)
+        del gui.session_state[task]
+        gui.rerun()
 
 gui.text_input(label='', placeholder="Add a new task...",
                on_change=add_task, key='new_task')
